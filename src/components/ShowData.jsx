ebd4216d261a50493520dea39cfc3530a50e9a68
Blake <32065552+ITDallasDBW@users.noreply.github.com>
2025-10-28 09:14:57 -0500

    brackets

--- src/components/ShowData.jsx
+++ src/components/ShowData.jsx
@@ -6,12 +6,12 @@ const ShowData = (dataToShow) => {
   return;
   <>
     <div className="movie">
-        {dataToShow.map((movie) => (
-            <div key={movie.imdbID}>
-                <h3>{movie.Title}</h3>
-                <p>{movie.Year}</p>
-                </div>
-        ))}
+      {dataToShow.map((movie) => (
+        <div key={movie.imdbID}>
+          <h3>{movie.Title}</h3>
+          <p>{movie.Year}</p>
+        </div>
+      ))}
       {/* {dataToShow.map((movie) => (
                 // key={movie.imdbID}>
                 <div className="movie__title">movie.Title}
